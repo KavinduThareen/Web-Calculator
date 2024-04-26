91b681e651c7c3ba9e15d7a2d8d49224751f50e7
thareen <kavindutharin@gmail.com>
2024-04-26 09:20:14 +0530

    edith js files

--- js/Calculator.js
+++ js/Calculator.js
@@ -11,3 +11,15 @@ function cleardisplay(){
 function calculate(){
     display.value = eval(display.value);
 }
+
+
+function backbtn() {
+    var display = document.getElementById("box-1");
+    var currentValue = display.value;
+   
+    if (currentValue.length > 0) {
+        var newValue = currentValue.slice(0, -1);
+        display.value = newValue;
+    }
+}
+
